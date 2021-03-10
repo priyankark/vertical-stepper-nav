5d5987d35f5ab1f5a2627bd2f93a6fde2360aa92
priykum <priykum@microsoft.com>
2021-03-10 15:23:11 +0530

    Add Step Component

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -1,25 +1,33 @@
 import * as React from 'react';
+import * as CSS from 'csstype';
+interface IStep {
+    stepContent: () => JSX.Element;
+    statusColor?: string;
+    statusCircleSize?: number;
+    onClickHandler?: (event?: React.MouseEvent<HTMLDivElement>) => void
+}
 
-// interface IStep {
-//     label: string;
-//     statusColor: string;
-// }
+const buttonContainerStyles: CSS.Properties = {
+    display: 'inline-flex',
+    flexWrap: 'wrap',
+    gap: "12px",
+    padding: "2px"
+};
 
-export const Step: any = () => {
-    return <div role="navigation">
-        <div style={{ display: 'inline-flex', flexWrap: 'wrap', gap: 12, padding: 5 }}>
-            <div
-                style={{
-                    borderRadius: 16,
-                    width: 16,
-                    height: 16,
-                    border: "2px solid #E1E1E1",
-                    background: "lightgreen"
-                }}
-            />
-            <div style={{ fontSize: 14, fontWeight: 'bold' }}>
-                Screen 1
-            </div>
+export const Step = ({ stepContent, statusColor, statusCircleSize, onClickHandler }: IStep) => {
+    const circleStyles = {
+        borderRadius: statusCircleSize ?? 16,
+        width: statusCircleSize ?? 16,
+        height: statusCircleSize ?? 16,
+        border: "2px solid #E1E1E1",
+        background: statusColor ?? "white"
+    };
+    return <div tabIndex={0} onClick={onClickHandler} role="button" style={{ ...buttonContainerStyles }}>
+        <div>
+            <div style={circleStyles} />
+        </div>
+        <div style={{ paddingBottom: 2 }}>
+            {stepContent()}
         </div>
     </div>
 }
