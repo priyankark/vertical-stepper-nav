4aece9761a7efdf78c63df48e0fb46722d63151b
priykum <priykum@microsoft.com>
2021-03-10 21:34:47 +0530

    Fix a11y linter issue

--- src/components/Step.tsx
+++ src/components/Step.tsx
@@ -23,19 +23,25 @@ export const Step = ({
   statusCircleSize,
   onClickHandler,
 }: IStep): JSX.Element => {
-  const circleStyles = React.useMemo(() => {
-    return {
-      borderRadius: statusCircleSize ?? 16,
-      width: statusCircleSize ?? 16,
-      height: statusCircleSize ?? 16,
-      border: '2px solid #E1E1E1',
-      background: statusColor ?? 'white',
-    };
-  }, [statusCircleSize, statusColor]);
+  const circleStyles = {
+    borderRadius: statusCircleSize ?? 16,
+    width: statusCircleSize ?? 16,
+    height: statusCircleSize ?? 16,
+    border: '2px solid #E1E1E1',
+    background: statusColor ?? 'white',
+  };
+
+  const keyDownHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      onClickHandler?.();
+    }
+  };
+
   return (
     <div
       tabIndex={0}
       onClick={onClickHandler}
+      onKeyDown={keyDownHandler}
       role="button"
       style={{...buttonContainerStyles}}>
       <div>
